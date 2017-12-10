78d7718259f80da09dd2518cc7ce146dfb388c13
Isaac Boates <iboates@gmail.com>
2017-12-10 12:56:13 +0100

    worked on database connection class - cannot write features because of problem in Polygon Constructor

--- src/tester/TestDatabaseConnection.java
+++ src/tester/TestDatabaseConnection.java
@@ -1,76 +1,79 @@
 package tester;
 
+import java.awt.*;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import core_classes.DatabaseConnection;
+import core_classes.Layer;
+import geometry.PolygonItem;
 
 /**
  * Created by isaac on 22/11/17.
  */
 public class TestDatabaseConnection {
 
-//    public static void main(String[] args) {
-//
-//        /**
-//         * Class for testing database functionality.
-//         *   |                                                     |
-//         *   |                                                     |
-//         *   |   PUT YOUR DATABASE CREDENTIALS HERE TO RUN TEST    |
-//         *   |                                                     |
-//         *   |                                                     |
-//         *   V                                                     v
-//         */
-//        // Initialize database connection
-//        DatabaseConnection databaseConnection = new DatabaseConnection(
-//            "localhost",
-//            5432,
-//            "softeng_db",
-//            "postgres",
-//            "postgres"
-//        );
-///*
-//        // create some new features and add them to ArrayLists
-//        ArrayList<String[]> redFeatures = new ArrayList<>();
-//        redFeatures.add(new String[] {"#ff0000", "1.5", "LINESTRING (30 10, 10 30, 40 40)"});
-//        redFeatures.add(new String[] {"#ff0000", "1.5", "LINESTRING (30 10, 10 30, 40 40)"});
-//        redFeatures.add(new String[] {"#ff0000", "1.5", "LINESTRING (30 10, 10 30, 40 40)"});
-//
-//        ArrayList<String[]> blueFeatures = new ArrayList<>();
-//        blueFeatures.add(new String[] {"#0000ff", "1.5", "LINESTRING (30 10, 10 30, 40 40)"});
-//        blueFeatures.add(new String[] {"#0000ff", "1.5", "LINESTRING (30 10, 10 30, 40 40)"});
-//        blueFeatures.add(new String[] {"#0000ff", "1.5", "LINESTRING (30 10, 10 30, 40 40)"});
-//
-//        ArrayList<String[]> greenFeatures = new ArrayList<>();
-//        greenFeatures.add(new String[] {"#00ff00", "1.5", "LINESTRING (30 10, 10 30, 40 40)"});
-//        greenFeatures.add(new String[] {"#00ff00", "1.5", "LINESTRING (30 10, 10 30, 40 40)"});
-//        greenFeatures.add(new String[] {"#00ff00", "1.5", "LINESTRING (30 10, 10 30, 40 40)"});
-//
-//        // write all the red features to a table called "red_table", and do the same respecitvely for blue beatures & green features
-//        databaseConnection.writeTable("red_table", redFeatures);
-//        databaseConnection.writeTable("blue_table", blueFeatures);
-//        databaseConnection.writeTable("green_table", greenFeatures);
-//
-//        // append the green features to the blue table
-//        databaseConnection.appendToTable("blue_table", greenFeatures);
-//
-//        // drop the green features altogether
-//        databaseConnection.dropTable("green_table");
-//
-//        // get all the tables in the database and print them to console
-//        ArrayList<String> tables = databaseConnection.getTables();
-//        for (int i=0; i<tables.size(); i++) {
-//
-//            System.out.println(tables.get(i));
-//
-//            ArrayList<String[]> features = databaseConnection.readTable(tables.get(i));
-//            for (int j=0; j<features.size(); j++) {
-//                System.out.println("\t" + features.get(j)[0]);
-//                System.out.println("\t" + features.get(j)[1]);
-//                System.out.println("\t" + features.get(j)[2]);
-//            }
-//
-//        }
-//*/
-//    }
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        /**
+         * Class for testing database functionality.
+         *   |                                                     |
+         *   |                                                     |
+         *   |   PUT YOUR DATABASE CREDENTIALS HERE TO RUN TEST    |
+         *   |                                                     |
+         *   |                                                     |
+         *   V                                                     v
+         */
+        // Initialize database connection
+        DatabaseConnection databaseConnection = new DatabaseConnection(
+            "localhost",
+            5432,
+            "softeng_db",
+            "postgres",
+            "postgres"
+        );
+
+        PolygonItem poly0 = new PolygonItem(0, new Polygon(new int[] {0,1,1,0}, new int[] {0,0,1,0}, 4));
+        PolygonItem poly1 = new PolygonItem(1, new Polygon(new int[] {0,2,2,0}, new int[] {0,0,2,0}, 4));
+        PolygonItem poly2 = new PolygonItem(2, new Polygon(new int[] {0,3,3,0}, new int[] {0,0,3,0}, 4));
+
+        Layer myLayer = new Layer(0,true,"Polygon","myLayer");
+
+        myLayer.getListOfFeatures().add(poly0);
+        myLayer.getListOfFeatures().add(poly1);
+        myLayer.getListOfFeatures().add(poly2);
+
+        for (int i=0; i<myLayer.getListOfFeatures().size(); i++) {
+            double[] curx = myLayer.getListOfFeatures().get(i).getCoordinatesArrayXY()[0];
+            double[] cury = myLayer.getListOfFeatures().get(i).getCoordinatesArrayXY()[1];
+            for (int j=0; j<curx.length; i++) {
+                System.out.print(curx[j] + ", ");
+                System.out.println(cury[j]);
+            }
+        }
+
+        // append the green features to the blue table
+        databaseConnection.appendToTable("test_table", myLayer);
+
+        // drop the green features altogether
+/*        databaseConnection.dropTable("green_table");
+
+        // get all the tables in the database and print them to console
+        ArrayList<String> tables = databaseConnection.getTables();
+        for (int i=0; i<tables.size(); i++) {
+
+            System.out.println(tables.get(i));
+
+            ArrayList<String[]> features = databaseConnection.readTable(tables.get(i));
+            for (int j=0; j<features.size(); j++) {
+                System.out.println("\t" + features.get(j)[0]);
+                System.out.println("\t" + features.get(j)[1]);
+                System.out.println("\t" + features.get(j)[2]);
+            }
+
+        }
+*/
+    }
 
 }
